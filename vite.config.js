7d08a78467a563f1039f85b65c4fa9495b76376c
Jason Leibel <jason.leibel@hotmail.com>
2025-06-19 21:18:46 -0600

    chore: more configuration updates

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0', // 👈 expose Vite outside container
-        port: 5173,      // 👈 lock to 5173 so Laravel knows where to find it
-        strictPort: true // 👈 fail if taken — better for dev/debug
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -15,4 +12,12 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'bootstrap/nginx/key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'bootstrap/nginx/cert.pem')),
+        },
+        host: 'yardageiq.local',
+        port: 5173,
+    },
 });
